df9cc62dc8b2b183f02a5b6d7bfee8ddd608f07a
Bruno Montebello <brunormontebello@hotmail.com>
2021-02-13 22:21:43 +0000

    Display card-list grid

--- mosnters-rolodex/src/App.js
+++ mosnters-rolodex/src/App.js
@@ -13,20 +13,16 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ monsters: users }));
+    fetch('https://jsonplaceholder.typicode.com/users') //primeiro faz a busca dos dados de users
+    .then(response => response.json()) //depois transforma num JSON para que assuma o formato q precisamos
+    .then(users => this.setState({ monsters: users })); //depois passa a key/value pair monsters: users como parametros da funcao setState, que e responsavel por atualizar o state. Antes de setState (monsters era array vazio; Depois (monsters: users)
   }
 
 
   render(){
     return (
       <div className="App">
-        <CardList name='Bruno'>
-        { this.state.monsters.map(monster => (
-          <h1 key={monster.id}>{ monster.name }</h1>
-        ))}
-        </CardList>
+        <CardList monsters={this.state.monsters}/>
       </div>
     );
   }
