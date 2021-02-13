45afbebcee2712011a80559cafc72f28c5644c01
Bruno Montebello <brunormontebello@hotmail.com>
2021-02-13 12:36:32 +0000

    Add card-list style

--- mosnters-rolodex/src/App.js
+++ mosnters-rolodex/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -22,6 +22,7 @@ class App extends Component {
   render(){
     return (
       <div className="App">
+        <CardList name="Brunaaaaao"/>
         {
           this.state.monsters.map(monster => <h1 key={monster.id}>{ monster.name }</h1>)
         }
